c74887bd50952ecfb2317984c55f82d91e01b91d
Anastasia <kulevich05@gmail.com>
2022-08-30 14:10:00 +0300

    Add choice of function to test.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,39 @@
 
 int main()
 {
-        test_strlen_f();
-        test_strcpy_f();
-        test_strchr_f();
-        test_strcmp_f();
-        test_strcat_f();
+        int choice = 0;
+
+        printf("Choose a function to test:\n"
+               "1) strlen_f()\n"
+               "2) scrcpy_f()\n"
+               "3) strchr_f()\n"
+               "4) strcmp_f()\n"
+               "5) strcat_f()\n");
+
+        while(scanf("%d", &choice)) {
+                while (getchar() != '\n')
+                        ;
+
+                switch (choice) {
+                        case 1:
+                                test_strlen_f();
+                                break;
+                        case 2:
+                                test_strcpy_f();
+                                break;
+                        case 3:
+                                test_strchr_f();
+                                break;
+                        case 4:
+                                test_strcmp_f();
+                                break;
+                        case 5:
+                                test_strcat_f();
+                                break;
+                        default: 
+                                printf("Wrong input.\n");
+                }
+        }
 
         return 0;
 }
